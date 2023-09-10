eb9aead5b8960bc52293bc42cd359fc53bc47140
LUCY <wangecikariu.luc@gmail.com>
2023-09-10 16:12:33 +0300

    form Abstraction

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,34 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  const [formData, setFormData] = useState({
+    firstName: "John",
+    lastName: "Henry"
+   
+  });
 
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
+  function handleChange(event) {
+    const name = event.target.name;
+    let value = event.target.value;
+
+    if(event.target.type === "checkbox") {
+      value= value.target.checked;
   }
 
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
-  }
+  setFormData ({
+    ...formData,
+    [name]: value,
+  });
+}
 
+function handleSubmit(event) {
+  event.preventDefault();
+  console.log(formData);
+}
   return (
     <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
+      <input type="text" name= "firstName" onChange={handleChange} value={formData.firstName} />
+      <input type="text" name= "lastName" onChange={handleChange} value={formData.lastName} />
       <button type="submit">Submit</button>
     </form>
   );
